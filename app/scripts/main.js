ea9b2f06eb46b792d8bac85a9da56b03a6f40a0d
Alec Ortega <alecortega@live.com>
2015-02-19 03:05:54 -0500

    Added dev and designer section, started on portfolio
    - Added logo
    - Added dev and designer section
    - Added "My Work" section
    - Started on portfolio using isotope

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,7 @@
-'use strict';
-
-var LastfmAPI = require('lastfmapi');
-
-var lfm = new LastfmAPI({
-  'api_key' : '6f5612126bc766d04a8de77bcdc4403e',
-  'secret' : '6932b57b07162618ac5c8d5eedbcf6ea'
-});
-
-lfm.track.getInfo({
-  'artist' : 'Poliça',
-  'track' : 'Wandering Star'
-}, function (err, track) {
-  if (err) { throw err; }
-  console.log(track);
+var $container = $('#isotope');
+// init
+$container.isotope({
+  // options
+  itemSelector: '.item',
+  layoutMode: 'fitRows'
 });
