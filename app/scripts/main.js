2f5e5aab93db8e076f6c995ad2daaf3e586d0bed
Alec Ortega <alecortega@live.com>
2015-02-25 23:44:52 -0500

    Removed particles.js
    - Removed particles.js due to performance issues
    - Will add gear animation in place of

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,6 +11,9 @@ $container.isotope({
 // Initialize auto-resize of textarea element
 $('.message').autosize();
 
+// Initializes smooth scrolling to all a tags
+$('a').smoothScroll({offset: -$('#nav-trigger').innerHeight()});
+
 // Initialize NiceScoll
 $(document).ready(function() {
   $('html').niceScroll({
@@ -24,8 +27,10 @@ $(document).ready(function() {
 // Initialize ScrollMagic
 //TODO: Refactor height variables out
 var controller = new ScrollMagic();
+var devController = new ScrollMagic({ container: '#browser-developer-trigger' });
+var desController = new ScrollMagic({ container: '.designer' });
 
-var navHeight = $('#nav-trigger').innerHeight()-1;
+var navHeight = $('#nav-trigger').innerHeight();
 
 var navigationScene = new ScrollScene({ triggerElement: '#nav-trigger', triggerHook: 0, reverse: true})
   .setPin('#nav-element');
@@ -39,91 +44,19 @@ var navigationWorkScene = new ScrollScene({ triggerElement: '#work', offset: -na
 var navigationContactScene = new ScrollScene({ triggerElement: '.recommendations', offset: -navHeight, triggerHook: 0, duration: $('.recommendations').innerHeight()+$('.contact').innerHeight+$('footer').innerHeight(), reverse: true})
   .setClassToggle('.contact-link', 'highlight');
 
-var developerScene = new ScrollScene({ triggerElement: '#browser-developer-trigger', triggerHook: 0.15, reverse: true})
+var developerScene = new ScrollScene({ triggerElement: '#browser-developer-trigger', triggerHook: 0, reverse: true})
   .setPin('#browser-developer-element');
 
-var designerScene = new ScrollScene({ triggerElement: '#browser-designer-trigger', triggerHook: 0.15, reverse: true})
+var designerScene = new ScrollScene({ triggerElement: '#browser-designer-trigger', triggerHook: 0, reverse: true})
   .setPin('#browser-designer-element');
 
 controller.addScene([
   navigationScene,
-  developerScene,
-  designerScene,
 
   navigationAboutScene,
   navigationWorkScene,
   navigationContactScene
 ]);
 
-
-
-
-
-
-// TODO: Need to figure out a way to DRY this plugin up
-// Initialize Particle.js for "Developer" section
-particlesJS('developer-particles', {
-  particles: {
-    color: '#384552',
-    color_random: false,
-    shape: 'circle', // "circle", "edge" or "triangle"
-    opacity: {
-      opacity: 3,
-      anim: {
-        enable: false,
-        speed: 1,
-        opacity_min: 0,
-        sync: false
-      }
-    },
-    size: 2.5,
-    size_random: true,
-    nb: 100,
-    line_linked: {
-      enable_auto: true,
-      distance: 350,
-      //color: '#43474B',
-      color: '#384552',
-      opacity: 0.6,
-      width: 1,
-      condensed_mode: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 600
-      }
-    },
-    anim: {
-      enable: false,
-      speed: 0.5
-    }
-  },
-  interactivity: {
-    enable: false,
-    mouse: {
-      distance: 250
-    },
-    detect_on: 'canvas', // "canvas" or "window"
-    mode: 'grab', // "grab" of false
-    line_linked: {
-      opacity: 0.5
-    },
-    events: {
-      onclick: {
-        enable: true,
-        mode: 'push', // "push" or "remove"
-        nb: 4
-      },
-      onresize: {
-        enable: true,
-        mode: 'out', // "out" or "bounce"
-        density_auto: true,
-        density_area: 800 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
-      }
-    }
-  },
-  /* Retina Display Support */
-  retina_detect: true
-});
-
-
-$('a').smoothScroll({offset: -$('#nav-trigger').innerHeight()});
+devController.addScene(developerScene);
+desController.addScene(designerScene);
