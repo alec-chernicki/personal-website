801dab63135c4ceabaa3a5925f24a2a640ce838b
Alec Ortega <alecortega@live.com>
2015-03-30 23:33:10 -0400

    Made second navbar that appears above "About" section

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,7 +5,7 @@ $(window).load(function() {
 
 $(document).ready(function() {
   // Created variable so that offsetting based on navigation height will be more DRY
-  var navHeight = $('#nav-trigger').innerHeight();
+  var navHeight = $('.nav-trigger').innerHeight();
 
 //// Initialize Masonry Containers
 //  var $container = $('#isotope');
@@ -27,46 +27,55 @@ $(document).ready(function() {
 
   // ScrollMagic - Navigation
   // ----------------------------------------------
-  var controller = new ScrollMagic.Controller();
+  var navigationController = new ScrollMagic.Controller();
+
+  // Set navbar to visible right above the about section
   var navigationScene = new ScrollMagic.Scene({
-    triggerElement: '#nav-trigger',
+    triggerElement: '#about',
     triggerHook: 0,
-    offset: 0,
+    offset: -navHeight,
     reverse: true
   })
-    .setPin('#nav-element')
-    .addTo(controller);
+    .setClassToggle('.nav-trigger', 'visible')
+    .addTo(navigationController);
 
+  // Set scroll to "About Me" options
   new ScrollMagic.Scene({
     triggerElement: '#about',
-    offset: -navHeight,
+    offset: -(navHeight+1),
     triggerHook: 0,
     duration: $('#about').height() +
-    $('.developer').height() +
-    $('.designer').height(), reverse: true
+    $('#developer').height() +
+    $('#designer').height(),
+    reverse: true
   })
     .setClassToggle('.about-link', 'highlight')
-    .addTo(controller);
+    .addTo(navigationController);
 
+  // Set scroll to "My Work" options
   new ScrollMagic.Scene({
     triggerElement: '#work',
-    offset: -navHeight,
+    offset: -(navHeight+1),
     triggerHook: 0,
-    duration: $('#work').innerHeight() + $('#portfolio').innerHeight() - $('#contact').height() + navHeight,
+    duration: $('#work').height() +
+    $('#portfolio').height() -
+    $('#contact').height() +
+    navHeight,
     reverse: true
   })
     .setClassToggle('.work-link', 'highlight')
-    .addTo(controller);
+    .addTo(navigationController);
 
+  // Set scroll to "Contact Me" options
   new ScrollMagic.Scene({
     triggerElement: '#contact',
-    offset: -navHeight,
-    triggerHook: 1,
+    offset: -(navHeight+1),
+    triggerHook: 0.5,
     duration: $('#contact').height() + $('.footer').height(),
     reverse: true
   })
     .setClassToggle('.contact-link', 'highlight')
-    .addTo(controller);
+    .addTo(navigationController);
 
 
   // ScrollMagic - Skillset wipe effect
@@ -93,13 +102,13 @@ $(document).ready(function() {
   var browserDesignerController = new ScrollMagic.Controller();
 
   var designerTweenTimeline = new TimelineMax()
-    .add(TweenMax.to('.browser-designer-container', 1, {height: '690px', ease:Linear.easeNone}));
+    .add(TweenMax.to('.browser-designer-container', 1, {height: 670, ease:Linear.easeNone}));
 
   new ScrollMagic.Scene({
     triggerElement: '#designer',
     // Sets speed of wipe dynamically based on browser height
     duration: $('.browser-developer-container').height(),
-    offset: 420,
+    offset: (navHeight),
     triggerHook: 'onEnter'
   })
     .setTween(designerTweenTimeline)
@@ -143,12 +152,5 @@ $(document).ready(function() {
   //$(window).resize(function() {
   //  navigationScene.update();
   //});
-
-  $(window).on('resize', function () {
-    if($('#nav-trigger').children('.scrollmagic-pin-spacer')) {
-      console.log('nav changed');
-      $('#nav-element').css('top', '0')
-    }
-  });
 });
 
